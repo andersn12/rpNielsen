9c1c2739929946ae4972ab5285448b0fcfebf734
Chris Hanas <chanas@ctemc.org>
2023-11-13 12:35:25 -0500

    Created new CSV files to rename originals

--- js/graph.js
+++ js/graph.js
@@ -3,8 +3,8 @@
 */ 
 
 async function getData(){
-    const response = await fetch('/rpNielsen/data/trial_1.csv');
-    //const response = await fetch('../data/trial_1.csv');   // Double period moves up one folder
+    const response = await fetch('/rpNielsen/data/trial-1.csv');
+    //const response = await fetch('../data/trial-1.csv');   // Double period moves up one folder
     const data = await response.text();     // CSV is in text format
     // console.log(data);
 
